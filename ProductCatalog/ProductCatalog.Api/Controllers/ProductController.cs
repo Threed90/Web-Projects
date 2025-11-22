7b9abdb92dedf19b69b09ba863590d65b5cd6cb1
Threed90 <threed@abv.bg>
2025-11-22 15:46:11 +0200

    Adding API controller and swagger UI through slashbuckle

---
+++ ProductCatalog/ProductCatalog.Api/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Services.Interfaces;
+
+namespace ProductCatalog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        [HttpGet(Name = "All")]
+        public IActionResult GetAll()
+        {
+            return Ok(_productService.GetAll());
+        }
+    }
+}
--- ProductCatalog/ProductCatalog.Api/Program.cs
+++ ProductCatalog/ProductCatalog.Api/Program.cs
@@ -1,3 +1,6 @@
+using ProductCatalog.Services.Interfaces;
+using ProductCatalog.Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,13 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi("/api");
+    app.UseSwaggerUI(option =>
+    {
+        option.RoutePrefix = "";
+        option.SwaggerEndpoint("/api", "ProductCatalog.Api");
+    });
 }
 
 app.UseHttpsRedirection();
