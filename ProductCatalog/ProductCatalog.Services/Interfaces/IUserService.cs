ad80c860e4b90ce8c8b229892fa7db56d6a5201e
Threed90 <threed@abv.bg>
2026-01-21 18:05:36 +0200

    Adding UserService for userLogin logic

--- ProductCatalog/ProductCatalog.Services/Interfaces/IUserService.cs
+++ ProductCatalog/ProductCatalog.Services/Interfaces/IUserService.cs
@@ -6,6 +6,11 @@ namespace ProductCatalog.Services.Interfaces
 {
     public interface IUserService
     {
-        
+        Task<bool> Register(string username, string password, string confirmPass);
+
+        Task<bool> Login(string username, string password);
+
+        Task Logout(string? username);
+
     }
 }
---
+++ ProductCatalog/ProductCatalog.Services/Services/UserService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProductCatalog.Models.DB_Models;
+using ProductCatalog.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductCatalog.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserService(
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        public async Task<bool> Login(string username, string password)
+        {
+            var result = await this.signInManager.PasswordSignInAsync(username, password, false, false);
+
+            if (result.Succeeded)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task Logout(string? username)
+        {
+            var user = await this.userManager.FindByNameAsync(username);
+            if (user != null)
+            {
+                await this.signInManager.SignOutAsync();
+            }
+            
+        }
+
+        public async Task<bool> Register(string username, string password, string confirmPass)
+        {
+            if (password != confirmPass)
+            {
+                return false;
+            }
+
+            var user = await this.userManager.FindByNameAsync(username);
+            if (user != null)
+            {
+                return false;
+            }
+
+            var result = await this.userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = username,
+                Email = username
+            }, password);
+
+            if (result.Succeeded)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
